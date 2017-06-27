3fbbda5debde4905f425f519b972156ed176be1a
varun.vats <varun.vats@naukri.com>
2017-06-27 10:07:50 +0530

    add changes for adding new files and folders

--- notificationwrapper/models/daoFactory.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-var currentConfig = require ('../config/envConfig')();
-
-var dbVars = {
-	pool : mysql.createPool(currentConfig.mySQL),
-	notificationSections : {CVVIEWS : 1, WHTMA : 2}
-}
-module.exports = dbVars;
